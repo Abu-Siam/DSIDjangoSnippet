fb96f375112857b021f0f1d0df777841754f6c65
Abu-Siam <a.s.siamsiam@gmail.com>
2022-10-24 16:23:19 +0600

    added custom query in custom admin

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,5 +1,3 @@
 
 I
 nestedCrud/serializers.py,b/6/b6567fe6729d6b52e06779451e23079f8eabf03d
-B
-basicAuth/views.py,e/a/ea57761a294d9b66671bffc892b91dd77bb61dab
--- adminStackedInline/admin.py
+++ adminStackedInline/admin.py
@@ -15,6 +15,21 @@ class CartAdmin(admin.ModelAdmin):
     inlines = [ItemInline]
     list_display = ["created", "total_price", "paid"]
 
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ["name", "price"]
+
+    def get_queryset(self, request):
+        # return Item.objects.all()
+
+        if (request.user.is_superuser):
+            return Item.objects.all()
+        elif(request.user.groups.filter(name='tour company').exists()):
+            return Item.objects.filter(price__gt=10)
+        else:
+            return Item.objects.all()
+
 
 admin.site.register(Cart, CartAdmin)
-admin.site.register(Item)
+# admin.site.register(Item)
+admin.site.register(CartItem)
